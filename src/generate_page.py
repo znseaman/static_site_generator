fdf509d575266f28d90f8acfb5b93203cac68874
znseaman <znseaman@gmail.com>
2026-03-13 20:45:41 -0700

    Add build script to host static website on Github Pages

--- src/generate_page.py
+++ src/generate_page.py
@@ -13,7 +13,7 @@ def extract_title(markdown):
         
     raise Exception(f"This markdown {markdown} does not have a h1 header (#)")
 
-def generate_page(from_path, template_path, dest_path):
+def generate_page(from_path, template_path, dest_path, base_path):
     print(f" * {from_path} {template_path} -> {dest_path}")
     from_file = open(from_path, "r")
     markdown_content = from_file.read()
@@ -29,6 +29,8 @@ def generate_page(from_path, template_path, dest_path):
     title = extract_title(markdown_content)
     template = template.replace("{{ Title }}", title)
     template = template.replace("{{ Content }}", html)
+    template = template.replace("href=\"/", f"href=\"{base_path}")
+    template = template.replace("src=\"/", f"src=\"{base_path}")
 
     dest_dir_path = os.path.dirname(dest_path)
     if dest_dir_path != "":
@@ -36,12 +38,12 @@ def generate_page(from_path, template_path, dest_path):
     to_file = open(dest_path, "w")
     to_file.write(template)
 
-def generate_pages_recursive(dir_path_content, template_path, dest_dir_path):
+def generate_pages_recursive(dir_path_content, template_path, dest_dir_path, basepath):
     for filename in os.listdir(dir_path_content):
         from_path = os.path.join(dir_path_content, filename)
         dest_path = os.path.join(dest_dir_path, filename)
         if os.path.isfile(from_path):
             dest_path = Path(dest_path).with_suffix(".html")
-            generate_page(from_path, template_path, dest_path)
+            generate_page(from_path, template_path, dest_path, basepath)
         else:
-            generate_pages_recursive(from_path, template_path, dest_path)
+            generate_pages_recursive(from_path, template_path, dest_path, basepath)
--- src/main.py
+++ src/main.py
@@ -1,28 +1,33 @@
 import os
 import shutil
+import sys
 
 from copy_static import copy_files_recursive
 from generate_page import generate_pages_recursive
 
 working_directory = "."
 dir_path_static = "static"
+dir_path_docs = "docs"
 dir_path_public = "public"
 dir_path_content = "content"
 template_path = "template.html"
 
 def main():
+    print(f"verify sys.argv: {sys.argv}")
+    basepath = sys.argv[1] if len(sys.argv) > 1 else "/"
+        
     abs_working_dir = os.path.abspath(working_directory)
-    dir_path_public_abs_path = os.path.normpath(os.path.join(abs_working_dir, dir_path_public))
+    dir_path_docs_abs_path = os.path.normpath(os.path.join(abs_working_dir, dir_path_docs))
 
-    if os.path.exists(dir_path_public_abs_path):
-        print("Deleting public directory...")
-        shutil.rmtree(dir_path_public_abs_path)
+    if os.path.exists(dir_path_docs_abs_path):
+        print(f"Deleting {dir_path_docs} directory...")
+        shutil.rmtree(dir_path_docs_abs_path)
 
     print("Copying static files to public directory...")
-    copy_files_recursive(working_directory, dir_path_static, dir_path_public)
+    copy_files_recursive(working_directory, dir_path_static, dir_path_docs)
     
     print("Generating content...")
-    generate_pages_recursive(dir_path_content, template_path, dir_path_public)
+    generate_pages_recursive(dir_path_content, template_path, dir_path_docs, basepath)
 
 if __name__ == "__main__":
     main()
